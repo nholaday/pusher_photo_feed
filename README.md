ba08a9f53d02df425a90aa1092171652f52a63f9
Nic Holaday <nic@storeease.com>
2022-03-14 08:07:13 -0700

    add readme

---
+++ README.md
@@ -0,0 +1,6 @@
+## How to run
+1. Create and activate venv
+2. install requirements
+3. Create a `.env` file from env-example
+4. source your env file
+
--- photofeed/settings.py
+++ photofeed/settings.py
@@ -25,7 +25,7 @@ PUSHER_APP_CLUSTER = os.getenv('PUSHER_APP_CLUSTER')
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')'
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
